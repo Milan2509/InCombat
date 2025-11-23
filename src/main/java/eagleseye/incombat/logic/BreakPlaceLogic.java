faedff0516bfac8f00f18b2421d9c1c632db1b25
EyeOfTheEagle <milan@milanverstappen.nl>
2025-11-23 20:27:50 +0100

    updated mod version to 1.0.2, fixed block placing issues

--- src/main/java/eagleseye/incombat/logic/BreakPlaceLogic.java
+++ src/main/java/eagleseye/incombat/logic/BreakPlaceLogic.java
@@ -4,6 +4,7 @@ import eagleseye.incombat.InCombat;
 import eagleseye.incombat.api.CombatCheck;
 import net.fabricmc.fabric.api.event.player.AttackBlockCallback;
 import net.fabricmc.fabric.api.event.player.UseBlockCallback;
+import net.minecraft.item.BlockItem;
 import net.minecraft.util.ActionResult;
 
 public class BreakPlaceLogic {
@@ -17,7 +18,11 @@ public class BreakPlaceLogic {
 
         // Block Placing
         UseBlockCallback.EVENT.register(((player, world, hand, blockHitResult) -> {
-            if(CombatCheck.isPlayerInCombat(player) && InCombat.COMBAT_CONFIG.disableBlockPlacing()) return ActionResult.FAIL;
+            if(CombatCheck.isPlayerInCombat(player) && InCombat.COMBAT_CONFIG.disableBlockPlacing()) {
+                if(player.getStackInHand(hand).getItem() instanceof BlockItem) return ActionResult.FAIL;
+
+                else return ActionResult.PASS;
+            }
 
             return ActionResult.PASS;
         }));
