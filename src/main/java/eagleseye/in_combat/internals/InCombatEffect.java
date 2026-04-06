60e58544e6ac2e977412a2acf1d3e7820c12f213
EyeOfTheEagle <milan@milanverstappen.nl>
2026-04-06 20:01:32 +0200

    improved apply effect logic

--- src/main/java/eagleseye/in_combat/internals/InCombatEffect.java
+++ src/main/java/eagleseye/in_combat/internals/InCombatEffect.java
@@ -1,10 +1,16 @@
 package eagleseye.in_combat.internals;
 
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectCategory;
 
 public class InCombatEffect extends StatusEffect {
     public InCombatEffect() {
         super(StatusEffectCategory.NEUTRAL, 0xe9b8b3);
+    }
+
+    @Override
+    public boolean applyUpdateEffect(LivingEntity entity, int amplifier) {
+        return true;
     }
 }
