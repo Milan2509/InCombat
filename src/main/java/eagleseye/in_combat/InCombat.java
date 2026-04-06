0af8bf07934b417f777119a53e30b04963ac1e28
EyeOfTheEagle <milan@milanverstappen.nl>
2026-04-06 13:52:06 +0200

    initial 1.21.1 commit

---
+++ src/main/java/eagleseye/in_combat/InCombat.java
@@ -0,0 +1,15 @@
+package eagleseye.in_combat;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class InCombat implements ModInitializer {
+	public static final String MOD_ID = "in_combat";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
