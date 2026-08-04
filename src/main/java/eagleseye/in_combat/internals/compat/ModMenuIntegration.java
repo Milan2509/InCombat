883cdea337e2fed8e240e3278fd4dcd77a11e9a0
EyeOfTheEagle <milan@milanverstappen.nl>
2026-08-04 17:32:55 +0200

    some last fixes

--- src/main/java/eagleseye/in_combat/internals/compat/ModMenuIntegration.java
+++ src/main/java/eagleseye/in_combat/internals/compat/ModMenuIntegration.java
@@ -1,19 +1,5 @@
 package eagleseye.in_combat.internals.compat;
 
-import com.terraformersmc.modmenu.api.ConfigScreenFactory;
-import com.terraformersmc.modmenu.api.ModMenuApi;
-import me.shedaniel.clothconfig2.api.ConfigBuilder;
-import net.minecraft.text.Text;
+public class ModMenuIntegration{
 
-public class ModMenuIntegration implements ModMenuApi {
-//    @Override
-//    public ConfigScreenFactory<?> getModConfigScreenFactory() {
-//        return parent -> {
-//            ConfigBuilder builder = ConfigBuilder.create()
-//                    .setParentScreen(parent)
-//                    .setTitle(Text.translatable("title.in_combat.config"));
-//
-//            return builder.build();
-//        };
-//    }
 }
